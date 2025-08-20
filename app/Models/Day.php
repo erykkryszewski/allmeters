705d80afa5e03cbd5c071868dc792acb6e125117
Eryk Kryszewski <eric9972@o2.pl>
2025-08-20 19:54:10 +0200

    chapter 2 finished

---
+++ app/Models/Day.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model; // allows to use simpler sql queries using php
+
+class Day extends Model
+{
+    // fields we can fill in bulk
+    protected $fillable = [
+        'date',
+        'work_hours',
+        'sleep_hours',
+        'calories',
+        'high_protein',
+        'low_fat',
+        'activity_level',
+        'wife_time',
+        'social_media_level',
+        'smoking',
+        'alcohol',
+        'other_drugs',
+        'meditation',
+        'reading',
+        'chess_games',
+        'water_level',
+        'score'
+    ];
+}
--- database/migrations/2025_08_20_173234_create_days_table.php
+++ database/migrations/2025_08_20_173234_create_days_table.php
@@ -13,9 +13,34 @@ return new class extends Migration
     {
         Schema::create('days', function (Blueprint $table) {
             $table->id();
+            $table->date('date')->unique();
+
+            $table->integer('work_hours')->default(0);
+            $table->decimal('sleep_hours', 3, 1)->default(0); // e.g. 7.5
+            $table->integer('calories')->nullable();
+            $table->boolean('high_protein')->default(false);
+            $table->boolean('low_fat')->default(false);
+
+            $table->enum('activity_level', ['none','short','long'])->default('none');
+            $table->boolean('wife_time')->default(false);
+            $table->enum('social_media_level', ['ok','medium','high'])->default('ok');
+
+            $table->boolean('smoking')->default(false);
+            $table->boolean('alcohol')->default(false);
+            $table->boolean('other_drugs')->default(false);
+
+            $table->boolean('meditation')->default(false);
+            $table->boolean('reading')->default(false);
+            $table->integer('chess_games')->default(0);
+
+            $table->enum('water_level', ['low','medium','high'])->default('low');
+
+            $table->integer('score')->default(0);
+
             $table->timestamps();
         });
     }
+
 
     /**
      * Reverse the migrations.
